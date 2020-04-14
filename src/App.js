656a12e326ae1c9030e7a645ce0c21e73d82147c
Andre <ajparker000@gmail.com>
2020-04-13 21:58:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Logo from './components/Logo/Logo';
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
+const particleOptions = {
+  particles: {
+    number: {
+      value: 75,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Particles className='particles' params={particleOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*      
+        <FaceRecognition /> */}
     </div>
   );
-}
+};
 
 export default App;
